9e0bf0e978ca2af9a260750fde7a16a4c0cbf51b
glowstonesq1 <reachglowstones@gmail.com>
2026-06-10 11:26:46 +0530

    KidsTab.jsx

--- src/components/admin/KidsTab.jsx
+++ src/components/admin/KidsTab.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { supabase, adminCreateUser } from '../../lib/supabase'
+import { supabase } from '../../lib/supabase'
 import useStore from '../../store/useStore'
 import { formatINR, timeAgo } from '../../lib/formatCurrency'
 import TransactionList from '../shared/TransactionList'
